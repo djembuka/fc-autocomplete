2d699ab64cc29659c71af4718ffa804e66c53a36
Tatiana <djembuka@gmail.com>
2014-09-21 15:41:36 +0400

    Create jquery project with grunt. Original files - src/origin

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-fc-autocomplete
-===============
+# Foodclub autocomplete jquery plugin
 
-Autocomplete jQuery plugin for Foodclub project. Ajax download, subsections, illustrations, recipe properties, en/ru suggestions
+Based on jquery ui widget factory
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/Tatiana/fc-autocomplete/master/dist/fc-autocomplete.min.js
+[max]: https://raw.github.com/Tatiana/fc-autocomplete/master/dist/fc-autocomplete.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/fc-autocomplete.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery.fc-autocomplete.js
@@ -0,0 +1,38 @@
+/*
+ * fc-autocomplete
+ * https://github.com/djembuka/fc-autocomplete
+ *
+ * Copyright (c) 2014 Tatiana
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.fc_autocomplete = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.fc_autocomplete = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.fc_autocomplete.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.fc_autocomplete.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].fc_autocomplete = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
