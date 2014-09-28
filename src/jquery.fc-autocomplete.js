b0184066deb093c6a818fa624b4569b72693a941
Tatiana <djembuka@gmail.com>
2014-09-28 21:21:30 +0400

    New code style for old functionality

--- src/jquery.fc-autocomplete.js
+++ src/jquery.fc-autocomplete.js
@@ -7,32 +7,128 @@
  */
 
 (function($) {
+  'use strict';
+  
+  // AUTOCOMPLETE PUBLIC CLASS DEFINITION
+  // ===============================
 
-  // Collection method.
-  $.fn.fc_autocomplete = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  var Autocomplete = function ( element, options ) {
+    this.options    =
+    this.enabled    =
+    this.$element   = null;
+
+    this.init( element, options );
+  };
+  
+  Autocomplete.DEFAULTS = {
+			itemsNum : {
+			recipes: 5,
+			dishes: 3,
+			ingredients: 3,
+			cuisines: 3
+		},
+		blockTitles: [
+			"recipes",
+			"dishes",
+			"ingredients",
+			"cuisines"
+		],
+		itemClass: "b-rs__item",
+		linkClass: "b-rs__item__link",
+		headingClass: "b-rs__heading",
+		strClass: "b-rs-str",
+		searchAllClass: "b-rs__search-all",
+		itemTag: "li"
+  };
+  
+  Autocomplete.prototype.init = function ( element, options ) {
+    this.enabled   = true
+    this.$element  = $( element );
+	  this.options = this.getOptions( options );
+		this.$input = this.$element.find(".b-recipe-search__input");
+		this.$activeItem = this.$input;// active list item (highlighted by keyboard arrows)
+		this.elemValue = $.trim( this.$input.val() );
+		//this.$delete = this.$elem.find(".b-recipe-search__delete");
+		//this.$button = this.$elem.find(".b-recipe-search__button");
+		// ! Create option for additional form elements like delete and send buttons.
+  };
+
+  Autocomplete.prototype.getDefaults = function () {
+    return Autocomplete.DEFAULTS;
+  };
+
+  Autocomplete.prototype.getOptions = function (options) {
+    options = $.extend( {}, this.getDefaults(), this.$element.data(), options );
+    return options;
+  };
+  
+  Autocomplete.prototype.getOption = function( option ) {
+    return this.options[ option ];
+  };
+  
+  Autocomplete.prototype.setOption = function( option, value ) {
+	this.options[ option ] = value;
+  };
+  
+  Autocomplete.prototype.destroy = function() {
+	this.$element.off( ".fc_autocomplete" ).removeData( "fc_autocomplete" );
+  };
+  
+  // AUTOCOMPLETE PLUGIN DEFINITION
+  // =========================
+
+  function Plugin( option, params ) {
+	
+	//If call the plugin for an option value
+	var result;
+	
+	if ( typeof option === 'string' ) {
+	  this.each( function() {
+		  var $this = $( this ),
+			  data = $this.data( "fc_autocomplete" );
+		  
+		  if ( !data ) return;
+		  
+		  if ( typeof data[option] === "function" ) {
+				data[ option ]( params );
+			
+		  } else if ( params ) {
+				data.setOption( option, params );
+			
+		  } else {
+			  result = data.getOption( option ) || "";
+		  }      
+		});
+		
+		if ( result || result === "" ) return result;
+		return this;
+	}
+	
+	//If call for method or a new instance
+    return this.each( function () {
+      var $this   = $( this ),
+	      data    = $this.data( 'fc_autocomplete' ),
+        options = typeof option === 'object' && option;
+	  
+      if ( !data ) {
+	      $this.data( 'fc_autocomplete',
+	        ( data = new Autocomplete( this, options ) ));
+      }
     });
-  };
+  }
 
-  // Static method.
-  $.fc_autocomplete = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.fc_autocomplete.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
+  var old = $.fn.fc_autocomplete;
 
-  // Static method default options.
-  $.fc_autocomplete.options = {
-    punctuation: '.'
-  };
+  $.fn.fc_autocomplete             = Plugin;
+  $.fn.fc_autocomplete.Constructor = Autocomplete;
 
-  // Custom selector.
-  $.expr[':'].fc_autocomplete = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
 
-}(jQuery));
+  // AUTOCOMPLETE NO CONFLICT
+  // ===================
+
+  $.fn.fc_autocomplete.noConflict = function () {
+    $.fn.fc_autocomplete = old;
+    return this;
+  }
+
+}( jQuery ));
